f2a80cb92c1ea5b6d2b487cfdc047778c0726bb4
Marco C <m.colussi@miriade.it>
2023-11-09 14:00:06 +0100

    saved configurations for future use

---
+++ CONFIGURATION.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir /etc/systemd/system/ollama.service.d
+
+echo "[Service]
+Environment=OLLAMA_HOST=0.0.0.0:11434
+Environment=OLLAMA_ORIGINS=http://0.0.0.0:*,http://3.127.248.0:*" > /etc/systemd/system/ollama.service.d/environment.conf
+
+systemctl daemon-reload
+systemctl restart ollama
